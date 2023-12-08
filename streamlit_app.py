454333649cd1a6513fbbe3e23c7b234d7e6f84e1
Sachin Patel <79518245+sachin3098@users.noreply.github.com>
2023-12-08 15:45:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
 def find_largest_number(num1, num2, num3):
-    return max(num1, num2, num3)
+    # Find the largest number among the three
+    largest_number = max(num1, num2, num3)
+    return largest_number
 
 def main():
-    st.title("Find the Largest Number")
+    st.title("Find the Largest Number App")
 
-    # Input for three numbers
-    num1 = st.number_input("Enter the first number", value=0)
-    num2 = st.number_input("Enter the second number", value=0)
-    num3 = st.number_input("Enter the third number", value=0)
+    # Input three numbers
+    num1 = st.number_input("Enter the first number:")
+    num2 = st.number_input("Enter the second number:")
+    num3 = st.number_input("Enter the third number:")
 
-    # Button to calculate the largest number
-    if st.button("Find Largest Number"):
-        largest_number = find_largest_numb
+    # Display the input numbers
+    st.write("You entered the numbers:")
+    st.write("Number 1:", num1)
+    st.write("Number 2:", num2)
+    st.write("Number 3:", num3)
+
+    # Find and display the largest number
+    largest_number = find_largest_number(num1, num2, num3)
+    st.write("The largest number is:", largest_number)
+
+if __name__ == "__main__":
+    main()
