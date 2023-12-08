ccd8500eede008d67340abef413cf0a72ea42ebb
Sachin Patel <79518245+sachin3098@users.noreply.github.com>
2023-12-08 15:51:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,6 @@ def main():
     num2 = st.number_input("Enter the second number:")
     num3 = st.number_input("Enter the third number:")
 
-    # Display the input numbers
-    st.write("You entered the numbers:")
-    st.write("Number 1:", num1)
-    st.write("Number 2:", num2)
-    st.write("Number 3:", num3)
-
     # Find and display the largest number
     largest_number = find_largest_number(num1, num2, num3)
     st.write("The largest number is:", largest_number)
